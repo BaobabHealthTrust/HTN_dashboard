60064583f9fad831368b6d6057805bb68bec931c
mwatha <mwathabwanali@gmail.com>
2016-01-13 13:40:56 +0200

    adding reports

--- lib/bean.rb
+++ lib/bean.rb
@@ -2,7 +2,7 @@ class PatientBean
 
  attr_accessor :given_name, :middle_name, :family_name, :gender, :birthdate, 
                :cell_phone_number, :office_phone_number, :home_phone_number, 
-               :old_identification_number, :patient_id, :age, :visit_type,:vitals_present
+               :old_identification_number, :patient_id, :age, :visit_type,:skipped_vitals
 
 	def initialize(name)
 		@name = name
